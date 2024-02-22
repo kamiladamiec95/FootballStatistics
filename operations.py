eeabe63688bee07d7677f580ee475dcb2c0585eb
Kamil Adamiec <kamiladamiec95@wp.pl>
2024-02-22 22:02:17 +0100

    Add operations file

--- operations.py
+++ operations.py
@@ -1,5 +1,89 @@
-def change_file_path():
-    path = input("Files path: ")
+import os
+import re
+import json
+import db
 
-    with open("config.csv", "w") as f:
-        f.write(path)
+CONFIG_FILE = "config.json"
+
+def read_data_from_json(file_path):
+    result = []
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    for v in data.values():
+        result.append(v)
+    return result
+
+def add_read_data_to_db():
+    data_from_file = read_data_from_json("test1.json")
+    for data in data_from_file:
+            # db.add_event(event["event_name"], event["event_description"], event["player"], event["team"], event["match_time"])
+        # print(data["event_name"], data["event_description"], data["player"], data["team"], data["match_time"])
+        # print(data["events"])
+        for event in data["events"]:
+            db.add_event(event["event_name"], event["event_description"], event["player"], event["team"], event["match_time"], event["league"])
+
+
+def add_leagues_from_file():
+    with open(CONFIG_FILE, "r") as f:
+        config_data = json.load(f)
+
+    leagues_to_add = config_data["leagues_to_add"]
+    current_leagues = config_data["leagues"]
+
+    for league in leagues_to_add:
+        db.add_leagues(league)
+
+    current_leagues.extend(leagues_to_add)
+
+    config_data["leagues_to_add"] = []
+    config_data["leagues"] = current_leagues
+
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(config_data, f)
+
+def add_teams_from_file(files_path):
+    with open(CONFIG_FILE, "r") as f:
+        config_data = json.load(f)
+
+    leagues = config_data["leagues"]
+
+    for file in os.listdir(files_path):
+        filename = os.fsdecode(file)
+        if filename.replace("Teams.json", "") in leagues and filename.endswith("Teams.json"):
+            full_file_path = f"{files_path}/{filename}"
+            teams = read_data_from_json(full_file_path)
+
+add_teams_from_file("Teams")
+
+# add_leagues_from_file()
+# add_leagues_from_file()
+
+# db.add_leagues(['Premier League', 'Bundesliga'])
+
+# d = read_data_from_json("test1.json")
+# for i in d:
+#     print(i["events"])
+
+# def get_config_data():
+#     result = {}
+
+#     with open(CONFIG_FILE, "r") as f:
+#         result = json.load(f)
+#     return result
+
+# def change_files_path():
+#     path = input("Files path: ")
+
+#     with open(CONFIG_FILE, "w") as f:
+#         try:
+#             get_config_data()
+#         except FileNotFound:
+            
+#         f.write(path)
+
+
+# def get_files_path():
+#     with open(CONFIG_FILE, "r") as f:
+#         files_path = f.read()
+
+
