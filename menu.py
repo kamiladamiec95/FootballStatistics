3140e90b10b2fd3cd26b5201b57d269c34862514
Kamil Adamiec <kamiladamiec95@wp.pl>
2024-02-29 20:48:24 +0100

    Change menu

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 menu = (
     "1. Import events files\n"
-    "2. Import matches files\n"
+    "2. Import teams files\n"
     "3. Change files' folder path\n"
     "4. Add leagues\n"
     "5. Quit\n"
